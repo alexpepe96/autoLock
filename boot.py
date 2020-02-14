dc5bc24c06d45bc22f8df66836fe363fa6c2a0ad
alexpepe96 <33986340+alexpepe96@users.noreply.github.com>
2020-02-14 20:49:27 +0100

    First Update

---
+++ boot.py
@@ -0,0 +1,40 @@
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network
+import usocket
+import utime
+import picoweb
+import ulogging
+import ujson
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'Modena_100FX'
+password = 'fxbio_L0r&nz0'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+station.ifconfig(('192.168.1.193', '255.255.255.0', '192.168.1.1', '8.8.8.8'))
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+eLock=Pin(27,Pin.OUT,value=0)
+rack1=Pin(25,Pin.IN)
+rack2=Pin(26,Pin.IN)
+rack3=Pin(32,Pin.IN)
+rack4=Pin(33,Pin.IN)
+
+racks=[rack1,rack2,rack3,rack4]
---
+++ main.py
@@ -0,0 +1,32 @@
+app = picoweb.WebApp(__name__)
+
+@app.route('/tennisify/api/v1.0/lock', methods=['POST'])
+def post_lock_status(req,resp):
+    try:
+        eLock.value(1)
+        utime.sleep_ms(1000)
+        eLock.value(0)
+    except:
+        yield from picoweb.start_response(resp,status='500')
+        yield from resp.awrite("failed to set given status. Retry.")
+
+    yield from picoweb.start_response(resp,content_type='application/json')
+    yield from resp.awrite(ujson.dumps("success"))
+
+@app.route('/tennisify/api/v1.0/rack', , methods=['POST'])
+def get_rack_status(req,resp):
+    status={}
+    i=1
+    for pin in racks:
+        status['status_rack_{}'.format(i)]=pin.value()
+        i+=1
+    if not any(status.values()):
+        yield from picoweb.start_response(resp,status='500')
+        yield from resp.awrite("No rack sensor connected")
+    else:        
+        yield from picoweb.start_response(resp,content_type='application/json')
+        yield from resp.awrite(ujson.dumps(status)) 
+
+ulogging.basicConfig(level=ulogging.INFO)
+
+app.run(debug=True, host="192.168.1.193")
